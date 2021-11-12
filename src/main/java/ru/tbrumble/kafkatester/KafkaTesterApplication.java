5e245b7cf5d6bfd4778ff79fb83d316c33d18cc0
Konstantin Remizov <tbrumble@yandex.ru>
2021-11-12 16:58:41 +0300

    init commit

---
+++ src/main/java/ru/tbrumble/kafkatester/KafkaTesterApplication.java
@@ -0,0 +1,13 @@
+package ru.tbrumble.kafkatester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tbrumble/kafkatester/KafkaTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tbrumble.kafkatester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
