897a11168b0b5c1c17f2499768fa7d3ba9737dc2
Konstantin Remizov <tbrumble@yandex.ru>
2021-12-17 22:07:25 +0300

    add jacoco

--- src/main/java/ru/tbrumble/kafkatester/TestClass.java
+++ src/main/java/ru/tbrumble/kafkatester/TestClass.java
@@ -2,9 +2,17 @@ package ru.tbrumble.kafkatester;
 
 public class TestClass {
     String string;
+
+
     @Deprecated
     public Integer getInt() {
         return 12;
     }
+
+
+    Integer getASDASDASDASD() {
+        return null;
+    }
+
 
 }
